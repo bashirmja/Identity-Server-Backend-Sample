39e2a98d71f0585c68c9a7c73257db4f9b0b4e45
Bashir Momen <bashir.momen@amcsgroup.com>
2020-01-17 11:48:16 +0100

    Change mvc application to application with authontication

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Authorize]
+        public async Task<IActionResult> WeatherForecastAsync()
+        {
+            string apiUrl = "https://localhost:44316/weatherforecast";
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(apiUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var emp = new List<WeatherForecast>();
+            var responseMessage = await client.GetAsync(apiUrl);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+                emp = JsonConvert.DeserializeObject<List<WeatherForecast>>(responseData);
+            }
+            return View(emp);
+        }
+
+        public async Task<IActionResult> WeatherForecastOneAsync()
+        {
+            string apiUrl = "https://localhost:44316/weatherforecast/1";
+
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(apiUrl);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var emp = new WeatherForecast();
+            var responseMessage = await client.GetAsync(apiUrl);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+                emp = JsonConvert.DeserializeObject<WeatherForecast>(responseData);
+            }
+            return View(emp);
+        }
+    }
+}
