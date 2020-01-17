984979c76d3e5dced9e4b393bd153cbfad7c6afa
Bashir Momen <bashir.momen@amcsgroup.com>
2020-01-17 14:57:37 +0100

    Added javascript client

--- JavaScriptclient/Program.cs
+++ JavaScriptclient/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace AngularWebApp
+namespace JavaScriptclient
 {
     public class Program
     {
