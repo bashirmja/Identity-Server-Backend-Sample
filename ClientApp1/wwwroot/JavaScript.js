300e2ee5c723bfff499fe68c111d4ed582d137c7
Bashir Momen <bashir.momen@amcsgroup.com>
2020-01-23 13:50:17 +0100

    better naming and formating

--- ClientApp1/wwwroot/JavaScript.js
+++ ClientApp1/wwwroot/JavaScript.js
@@ -4,6 +4,10 @@
 
 var myApp = function () {
 
+    return {
+        initializePage: init,
+    };
+
     var userManager = null;
 
     function init() {
@@ -11,6 +15,17 @@ var myApp = function () {
         setupLoginMenu();
         setupDomEvents();
     };
+
+    function setupIodc() {
+        userManager = new Oidc.UserManager({
+            authority: "https://localhost:44361",
+            client_id: "js",
+            redirect_uri: "https://localhost:44336/callback.html",
+            response_type: "code",
+            scope: "openid profile customAPI.read",
+            post_logout_redirect_uri: "https://localhost:44336/index.html",
+        });
+    }
 
     function setupLoginMenu() {
         userManager.getUser().then(function (user) {
@@ -25,18 +40,6 @@ var myApp = function () {
                 $("#logoutmenu").css('visibility', 'hidden');
             }
         });
-
-    }
-
-    function setupIodc() {
-        userManager = new Oidc.UserManager({
-            authority: "https://localhost:44361",
-            client_id: "js",
-            redirect_uri: "https://localhost:44336/callback.html",
-            response_type: "code",
-            scope: "openid profile customAPI.read",
-            post_logout_redirect_uri: "https://localhost:44336/index.html",
-        });
     }
 
     function setupDomEvents() {
@@ -48,44 +51,39 @@ var myApp = function () {
             userManager.signoutRedirect();
         });
 
-        $("#search").click(function () {
+        $("#api").click(function () {
             apiRequestHandler("https://localhost:44316/weatherforecast", "#result")
         });
 
-        $("#searchbyid").click(function () {
+        $("#apibyid").click(function () {
             apiRequestHandler("https://localhost:44316/weatherforecast/1", "#result", true)
         });
     };
 
     function apiRequestHandler(url, element, autorized) {
         $(element).text("");
-        var returnData = null;
 
         if (autorized) {
-
             userManager.getUser().then(function (user) {
                 if (user) {
-                    returnData = ajaxCall(url, user.access_token)
+                    ajaxCall(url, user.access_token)
                         .then(function (data) {
                             $(element).text(data)
                         });
-
                 } else {
                     $(element).text("Access Denaid!")
                 }
             });
         }
         else {
-            returnData = ajaxCall(url)
+            ajaxCall(url)
                 .then(function (data) {
                     $(element).text(data)
                 });
         }
-
     }
 
     function ajaxCall(targetUrl, userToken) {
-
         return $.ajax({
             url: targetUrl,
             beforeSend: function (xhr) {
@@ -94,8 +92,4 @@ var myApp = function () {
             }
         });
     }
-
-    return {
-        initializePage: init,
-    };
 };
