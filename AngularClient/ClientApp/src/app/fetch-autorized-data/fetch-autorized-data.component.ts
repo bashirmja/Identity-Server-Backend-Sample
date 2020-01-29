a5f824a1a00f9af306b6bc64febd2f38600e63bd
Bashir Momen <bashir.momen@amcsgroup.com>
2020-01-29 11:39:23 +0100

    added autorized api call

---
+++ AngularClient/ClientApp/src/app/fetch-autorized-data/fetch-autorized-data.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-fetch-autorized-data',
+  templateUrl: './fetch-autorized-data.component.html'
+})
+export class FetchAutorizedDataComponent {
+  public forecasts: WeatherForecast[];
+
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>('https://localhost:44316/weatherforecast/1').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
